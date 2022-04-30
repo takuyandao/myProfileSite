7ca1216a66484af3ed499f265cd25051e90946b6
takuyandao <takuyanmaru1212@gmail.com>
2022-05-01 05:45:57 +0900

    create-about-section

---
+++ scripts/libs/scroll.js
@@ -0,0 +1,58 @@
+class ScrollEvent {
+
+    constructor() {
+        this.hero__title = document.querySelector('.hero__title');
+        this.header = document.querySelector('.header');
+        this._onScroll();
+    }
+
+    _onScroll() {
+        const fn = () => {
+            let value = scrollY / 10;
+            this.hero__title.style.marginTop = `${-value}%`;
+
+            if (scrollY > 650) {
+                this.header.classList.add('active');
+            } else {
+                this.header.classList.remove('active');
+            };
+        }
+
+        onscroll = fn;
+    }
+
+}
+
+class ScrollObserver {
+    constructor(els, cb, options) {
+
+        this.els = document.querySelectorAll(els);
+        this.cb = cb
+        const defaultOptions = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0,
+        }
+        this.options = Object.assign(defaultOptions, options);
+
+        this._init();
+    }
+
+    _init() {
+        const callback = (entries, observer) => {
+
+            entries.forEach(entry => {
+
+                if (entry.isIntersecting) {
+                    this.cb(entry.target);
+                    observer.unobserve(entry.target);
+                }
+
+            });
+        }
+
+        this.io = new IntersectionObserver(callback.bind(this), this.options);
+
+        this.els.forEach(el => this.io.observe(el));
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => { 
+
+    new ScrollEvent();
+
+    // hero title
+    const fn = (el) => {
+        const obj = new TextAnimation('.title-up');
+        obj.animate();
+    }
+    new ScrollObserver('.hero__title', fn);
+
+    // about title
+    const fn2 = (el) => {
+        const obj2 = new TextAnimation('.title-down');
+        obj2.animate();
+    }
+    new ScrollObserver('.title-down', fn2);
+
+    // cloud icon
+    const fn3 = (el) => {
+        el.classList.add('inview');
+    }
+    new ScrollObserver('.fa-cloud', fn3, { rootMargin: "0px 0px -100px 30px" });
+    
+    // about bar
+    const fn4 = (el) => {
+        el.classList.add('inview');
+    }
+    new ScrollObserver('.about__bar', fn4);
+
+});
+
 const btn = document.querySelector('.mobile-menu__btn');
 const container = document.querySelector('#global-container');
 const cover = document.querySelector('.mobile-menu__cover');
-
-new TextAnimation('.animate-title');
 
 btn.addEventListener('click', () => {
     container.classList.toggle('menu-open');
@@ -10,18 +40,3 @@ btn.addEventListener('click', () => {
 cover.addEventListener('click', () => {
     container.classList.toggle('menu-open');
 })
-
-const hero__title = document.querySelector('.hero__title');
-
-const fn = () => {
-    let value = scrollY / 10;
-    hero__title.style.marginTop = -value + '%';
-
-    if(scrollY > 700){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.header').classList.remove('active');
-    };
-}
-
-onscroll = fn;
