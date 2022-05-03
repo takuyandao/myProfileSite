94a39999ef04f8c41549f4a08582510e4f257e06
takuyandao <takuyanmaru1212@gmail.com>
2022-05-04 04:05:24 +0900

    ポートフォリオセクションの作成

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     new ScrollObserver('.title-up', fn);
 
-    // about h1, career h1 
+    // about h1, career h1, portfolio h1
     const fn2 = (el) => {
         const obj2 = new TextAnimation(el);
         obj2.animate();
     }
 
-    new ScrollObserver('.title-down', fn2);
+    new ScrollObserver('.title-down', fn2, { rootMargin: "0px 0px -50px 0px" });
 
     // cloud icon
     const fn3 = (el) => {
@@ -30,5 +30,17 @@ document.addEventListener('DOMContentLoaded', () => {
         el.classList.add('inview');
     }
     new ScrollObserver('.about__bar', fn4);
+
+    // portfolio__img
+    const fn5 = (el) => {
+        el.classList.add('inview');
+    }
+    new ScrollObserver('.portfolio__img', fn5, { rootMargin: "0px 0px -100px 3px" });
+
+    // portfolio__item
+    const fn6 = (el) => {
+        el.classList.add('inview');
+    }
+    new ScrollObserver('.portfolio__item', fn6, { rootMargin: "0px 0px -100px 2px" });
 
 });
